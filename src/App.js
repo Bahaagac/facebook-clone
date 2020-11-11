d3e79cef215cb02ff88040a5b0ee20a6fbebb65b
Bahaagac <bahaagac@gmail.com>
2020-11-11 20:01:12 +0300

    Header

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Components/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello clever programmers</h1>
+      <Header/>
+      {/* feed */}
+      {/* widgets */}
+
+    </div>
+  );
+}
+
+export default App;
