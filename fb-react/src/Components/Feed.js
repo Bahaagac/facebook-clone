8bc536e7ce5e6d7925d40112449b6789d63f4d92
Bahaagac <bahaagac@gmail.com>
2020-11-14 16:07:44 +0300

    MessageSender Update

--- fb-react/src/Components/Feed.js
+++ fb-react/src/Components/Feed.js
@@ -2,6 +2,8 @@ import React from 'react'
 import StoryReel from './StoryReel';
 import MessageSender from './MessageSender';
 
+
+
 const Feed = () => {
     return (
         <div className ='feed'>
--- fb-react/src/Components/MessageSender.js
+++ fb-react/src/Components/MessageSender.js
@@ -1,10 +1,58 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Avatar, Input} from '@material-ui/core';
+import './MessageSender.css';
+
+import VideocamIcon from '@material-ui/icons/Videocam';
+import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon'
 
 const MessageSender = () => {
+    const [ input, setInput] = useState('');
+    const  [image, setImage] = useState(null)
+
+    const handleChange = (e) => {
+        if(e.target.files[0]) {
+            setImage(e.target.files[0])
+        }
+    }
+
+    const handleSubmit = () => {
+        console.log('Submitting')
+    }
     return (
-        <div>
-            
-        </div>
+        <div className="messageSender">
+            <div className="messageSender__top">
+                <Avatar src="https://picsum.photos/200"/>
+                <form>
+                    <input type="text" 
+                        className="messageSender__input" 
+                        placeholder="What is on your mind?" 
+                        value={input} onChange={(e) => setInput(e.target.value)}>
+                    </input>
+                    <Input type="file" 
+                        className="messageSender__fileSelector" 
+                        onChange={handleChange}>
+                    </Input>
+
+                    <button onClick={handleSubmit} type="submit">Hidden Submit</button>
+                </form>
+            </div>
+
+            <div className="messageSender__bottom">
+                <div className="messageSender__option">
+                    <VideocamIcon style={{color: 'red'}} />
+                    <h3>Live Video</h3>
+                </div>
+                <div className="messageSender__option">
+                    <PhotoLibraryIcon stlye={{ color: 'green'}} />
+                    <h3>Photo/Video</h3>
+                </div>
+                <div className="messageSender__option">
+                    <InsertEmoticonIcon style= {{color:'orange'}} />
+                    <h3>Feeling/Activity</h3>
+                </div>
+            </div>
+        </div> 
     )
 }
 
